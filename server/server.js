f3db7c1f9d5acc9aa81869dd5b1e3ac7180b5801
koutamganesh <koutamganesh28@gmail.com>
2026-05-06 22:50:17 +0530

    railway fix

--- server/server.js
+++ server/server.js
@@ -1,36 +1,4 @@
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const cors = require("cors");
-// require("dotenv").config();
 
-// const app = express();
-
-// app.use(cors({
-//   origin: "http://localhost:3000",
-//   credentials: true
-// }));
-
-// app.use(express.json());
-
-// // TEST ROUTE (KEEP TOP)
-// app.get("/test", (req, res) => {
-//   res.send("Server is working");
-// });
-
-// // DB CONNECT
-// mongoose.connect(process.env.MONGO_URI)
-//   .then(() => console.log("DB Connected"))
-//   .catch(err => console.log(err));
-
-// // ROUTES
-// app.use("/api/auth", require("./routes/authRoutes"));
-// app.use("/api/projects", require("./routes/projectRoutes"));
-// app.use("/api/tasks", require("./routes/taskRoutes"));
-
-// // START SERVER
-// app.listen(8000, () => {
-//   console.log("Server running on 8000");
-// });
 
 const express = require("express");
 const mongoose = require("mongoose");
@@ -40,7 +8,10 @@ require("dotenv").config();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: [
+    "http://localhost:3000",
+    "https://your-netlify-app.netlify.app" 
+  ],
   credentials: true
 }));
 
@@ -61,6 +32,8 @@ app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/projects", require("./routes/projectRoutes"));
 app.use("/api/tasks", require("./routes/taskRoutes"));
 
-app.listen(process.env.PORT, () =>
-  console.log("Server running on", process.env.PORT)
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () =>
+  console.log("Server running on", PORT)
 );
